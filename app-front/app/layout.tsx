680bd56590b5d1de6d10267fcda11e7735715fe4
Jorge Gallardo <jorgito899@gmail.com>
2025-03-28 17:59:48 -0300

    feature: Se agregan cambios temporales
    Se está agregando la lógica de sesión en frontend

--- app-front/app/layout.tsx
+++ app-front/app/layout.tsx
@@ -9,6 +9,8 @@ import { siteConfig } from "@/config/site";
 import { fontSans } from "@/config/fonts";
 import { Navbar } from "@/components/navbar";
 
+import { AuthProvider } from "@/context/auth";
+
 export const metadata: Metadata = {
   title: {
     default: siteConfig.name,
@@ -32,6 +34,11 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
+  // Verificar si estamos logeados
+  // const isLoggedIn = useLoggedIn();
+  // if (!isLoggedIn) {
+  //   return <Login />;
+  // }
   return (
     <html suppressHydrationWarning lang="en">
       <head />
@@ -43,21 +50,9 @@ export default function RootLayout({
       >
         <Providers themeProps={{ attribute: "class", defaultTheme: "dark" }}>
           <div className="relative flex flex-col h-screen">
-            <Navbar />
-            <main className="container mx-auto max-w-7xl pt-16 px-6 flex-grow">
-              {children}
-            </main>
-            <footer className="w-full flex items-center justify-center py-3">
-              <Link
-                isExternal
-                className="flex items-center gap-1 text-current"
-                href="https://heroui.com?utm_source=next-app-template"
-                title="heroui.com homepage"
-              >
-                <span className="text-default-600">Powered by</span>
-                <p className="text-primary">HeroUI</p>
-              </Link>
-            </footer>
+            <AuthProvider>
+              <div>{children}</div>
+            </AuthProvider>
           </div>
         </Providers>
       </body>
