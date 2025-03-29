c6730195048ee74e6a978d2f711eef03bc924667
Jorge Gallardo <jorgito899@gmail.com>
2025-03-29 16:15:46 -0300

    feature: Se agrega CORSMiddleware

--- app-back/api.py
+++ app-back/api.py
@@ -4,9 +4,18 @@ from routers.auth import auth
 from routers.movements import movements
 from routers.product import product
 from routers.admin import admin
+from fastapi.middleware.cors import CORSMiddleware
 
 # Instanciamos la clase FastAPI
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Cambia "*" por el dominio específico si es necesario
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # Incluimos las rutas de autenticación
 app.include_router(auth.router)
